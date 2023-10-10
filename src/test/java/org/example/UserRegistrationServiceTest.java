9ad287aec84ba3bf235cd66c8363d4ecf31f3ab3
DianaGit1993 <diana.roscaneanu@yahoo.com>
2023-10-10 21:05:07 +0300

    Added tests

--- src/test/java/org/example/UserRegistrationServiceTest.java
+++ src/test/java/org/example/UserRegistrationServiceTest.java
@@ -1,7 +1,18 @@
 package org.example;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class UserRegistrationServiceTest {
 
+    UserRegistrationService userRegistrationService = new UserRegistrationService();
 
+    @Test
+    public void testRegisterUser(){
+        User userForTesting = User.builder().id(1).name("Jhon").build();
 
+        String result = userRegistrationService.registerUser(userForTesting);
+
+        Assertions.assertNotNull(result);
+    }
 }
