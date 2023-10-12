644ca77f47341067a3ca8ef5b12d4c7446da9f1b
DianaGit1993 <diana.roscaneanu@yahoo.com>
2023-10-12 21:27:56 +0300

    Unit tests

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Calculator {
+
+    public int addNumbers(int a, int b) {
+        return a + b;
+    }
+
+    public int multiplyNumbers(int a, int b) {
+        System.out.println("wefwef");
+        return a * b;
+    }
+
+}
