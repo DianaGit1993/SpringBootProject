06793364aa1d1b4cde51a730f3372969c2341644
David Vasile <vasiled027@gmail.com>
2023-10-18 17:48:30 +0300

    ...

--- src/main/java/org/example/UserRegistrationService.java
+++ src/main/java/org/example/UserRegistrationService.java
@@ -1,8 +1,16 @@
 package org.example;
 
+import java.util.Objects;
+
 public class UserRegistrationService {
 
-    public String registerUser(User user){
+    public String registerUser(gUser user){
+        if (Objects.isNull(user))
+        {
+            throw new IllegalArgumentException("User can not be null.");
+        }
+
+
         System.out.println("Registering new user "+ user.toString());
         return user.toString();
     }
--- src/test/java/org/example/UserRegistrationServiceTest.java
+++ src/test/java/org/example/UserRegistrationServiceTest.java
@@ -2,6 +2,10 @@ package org.example;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.Extensions;
+import org.junit.jupiter.api.parallel.Execution;
+
 
 public class UserRegistrationServiceTest {
 
@@ -14,5 +18,18 @@ public class UserRegistrationServiceTest {
         String result = userRegistrationService.registerUser(userForTesting);
 
         Assertions.assertNotNull(result);
+    }
+
+    @Test
+    public void testRegisterUserThrowsExceptionWhenUserIsNull(){
+        User userForTesting = null;
+
+       IllegalArgumentException exception =Assertions.
+               assertThrows(IllegalArgumentException.class,
+                       ()-> userRegistrationService.registerUser(userForTesting),
+                       "Expected IllegalArgumentException to be throw :(");
+
+        Assertions.assertTrue(exception.getMessage().contains("User can not be null."));
+        System.out.println("Test Succeded//");
     }
 }
