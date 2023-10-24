0ea09f5950ad2d3d47e869755a182b0ece0ed070
DianaGit1993 <diana.roscaneanu@yahoo.com>
2023-10-24 19:01:27 +0300

    Added resouce folder

--- src/main/java/org/example/controller/GreetingsController.java
+++ src/main/java/org/example/controller/GreetingsController.java
@@ -4,13 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class GreetingsController {
 
     @GetMapping("/greetings")
     public String greetings(){
         System.out.println("inside the greetings method");
-        return "Welcome ";
+        return "Welcome";
     }
 
     @GetMapping("/greetingsAndrei")
