2044507af134797a711efb1c9bff025b568c2de7
DianaGit1993 <diana.roscaneanu@yahoo.com>
2023-10-24 21:27:39 +0300

    Added rest controller

--- src/main/java/org/example/controller/RestaurantController.java
+++ src/main/java/org/example/controller/RestaurantController.java
@@ -2,8 +2,13 @@ package org.example.controller;
 
 import org.example.model.Restaurant;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Controller
 public class RestaurantController {
@@ -24,6 +29,14 @@ public class RestaurantController {
 
     List<Restaurant> restaurantList = List.of(restaurant1, restaurant2);
 
-
+    @GetMapping("/restaurant")
+    public String getRestaurants(Model model, @RequestParam(required = false) Integer id) {
+        if (Objects.isNull(id) || id.equals(0)) {
+            model.addAttribute("restaurants", restaurantList);
+        } else {
+            model.addAttribute("restaurants", restaurantList.stream().filter(x -> id == x.getId()).collect(Collectors.toList()));
+        }
+        return "restaurants";
+    }
 
 }
---
+++ src/main/java/org/example/controller/UserController.java
@@ -0,0 +1,28 @@
+package org.example.controller;
+
+import org.example.model.User;
+import org.example.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    UserController(UserService userService){
+        this.userService=userService;
+    }
+
+    @GetMapping("/getUsersByFirstName")
+    public List<User> getUsersByFirstName(@RequestParam String firstName){
+        return userService.findUsersByFirstName(firstName);
+    }
+
+}
