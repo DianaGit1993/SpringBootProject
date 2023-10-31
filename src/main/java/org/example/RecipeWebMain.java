e796028303bf3964fe8da2bd22200d4d37e96eb5
catalin-martazan <catalin.martazan@gmail.com>
2023-10-31 02:23:02 +0200

    Minor improvement

--- src/main/java/org/example/RecipeWebMain.java
+++ src/main/java/org/example/RecipeWebMain.java
@@ -1,14 +1,13 @@
 package org.example;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class RecipeWebMain {
 
-    private final static Logger log = LoggerFactory.getLogger(RecipeWebMain.class);
     public static void main(String[] args) {
         SpringApplication.run(RecipeWebMain.class);
     }
